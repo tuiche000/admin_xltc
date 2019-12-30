e7c950c8c819655567f61651329066182bbc3c37
caoshichao <845042291@qq.com>
2019-12-30 22:05:34 +0800

    d

--- src/pages/Web/components/header.js
+++ src/pages/Web/components/header.js
@@ -1,11 +1,52 @@
 import React from "react";
-import './common.css'
+import './common.css';
+import logo from '@/assets/images/logo.png';
+import { Divider, Row, Col } from 'antd';
 
 export default class Web extends React.Component {
   render() {
     return (
       <header>
+        <nav>
+          <Row type="flex" justify="space-between" align="bottom">
+            <Col>
+              <div className="logo">
+                <div>
+                  <img width="300" height="50" src={logo}></img>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <Row gutter={30} type="flex">
+                <Col>
+                  <a>首页</a>
+                </Col>
+                <Col>
+                  <a>应用案例</a>
+                </Col>
+                <Col>
+                  <a>使用方式</a>
+                </Col>
+                <Col>
+                  <a>技术支持</a>
+                </Col>
+                <Col>
+                  <a>版本日志</a>
+                </Col>
+              </Row>
+            </Col>
+            <Col>
+              <div className="nav_right">
+                <a>登录</a>
+                <Divider type="vertical"></Divider>
+                <a>注册</a>
+              </div>
+            </Col>
+          </Row>
 
+
+
+        </nav>
       </header>
     );
   }
